b5b858dc2118f150c4b51245ceeee328b0587d59
Ethan polley <ethanpolley@Ethans-MacBook-Pro.local>
2017-12-05 13:09:40 -0500

    Blah
    blah

--- MusicLibrary.java
+++ MusicLibrary.java
@@ -15,4 +15,26 @@ public class MusicLibrary {
     public static double[] trim(){
         
     }
+
+    public static double[] majorChord(double hz, double duration){
+        double majorThirdHz = hz * Math.pow(2,4/12);
+        double perfectFifthHz =hz * Math.pow(2,7/12);
+        double n = (double)StdAudio.SAMPLE_RATE;
+        int length =  (int)Math.ceil(n*duration);
+        System.out.println(length);
+        double[] root= new double[length];
+        double[] majorThird = new double[length];
+        double[] perfectFifth = new double[length];
+        for (int i = 0; i < length; i++) {
+                root[i] = Math.sin(2 * Math.PI * i * hz / StdAudio.SAMPLE_RATE);
+            }
+        for (int i = 0; i < length; i++) {
+                majorThird[i] = Math.sin(2 * Math.PI * i * majorThirdHz / StdAudio.SAMPLE_RATE);
+            }
+        for (int i = 0; i < length; i++) {
+                perfectFifth[i] = Math.sin(2 * Math.PI * i * perfectFifthHz / StdAudio.SAMPLE_RATE);
+            }
+        return ArrayTools.weightedAdd(ArrayTools.weightedAdd(root, majorThird,.5,.5),perfectFifth,2/3,1/3);
+    }
+}
 }
