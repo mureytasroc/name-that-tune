5e6106379590165c935b1ebd5b907c3bf8c87665
Charles Cunningham <mureytasroc@gmail.com>
2017-12-05 13:01:33 -0500

    edits

--- MusicLibrary.java
+++ MusicLibrary.java
@@ -1,6 +1,13 @@
-public class MusicLibrary{
-    public static void main(String args[]){
-        
-    }
-    
-}
+public class MusicLibrary {
+  public static void main(String args[]) {;}
+  public static double[] harmonic(double root, double duration) {
+   int N = duration*StdAudio.SAMPLE_RATE;
+      double[] smaller=new double[N];
+      double[] bigger=new double[N];
+      double[] norm=new double[N];
+   double low = root / 2;
+   for (double i = 0; i < duration; i+=1/StdAudio.SAMPLE_RATE) {
+       (Math.sin((2*Math.pi/root)*i)+Math.sin((2*Math.pi/root)*i)+Math.sin((2*Math.pi/root)*i))/3
+   }
+  }
+ }
