81cfdd2af1e73ead71a8c8bb0045b8b2c06153cb
Ethan polley <ethanpolley@Ethans-MacBook-Pro.local>
2017-12-06 11:43:07 -0500

    Major and Minor

--- MusicLibrary.java
+++ MusicLibrary.java
@@ -1,6 +1,6 @@
 public class MusicLibrary {
     public static void main(String args[]) {
-        StdAudio.play(majorChord(440,2));
+        StdAudio.play(minorChord(440,2));
     } 
 
     //Ethan's methods vv
@@ -14,6 +14,12 @@ public class MusicLibrary {
         return ArrayTools.addArrays(a, h, 0.5, 0.5);
     }
     */
+
+    /*
+
+    Function: Major Chord
+
+    */
     public static double[] majorChord(double hz, double duration){
         double majorThirdHz = hz * Math.pow(2,4.0/12);
         double perfectFifthHz =hz * Math.pow(2,7.0/12);
@@ -25,99 +31,38 @@ public class MusicLibrary {
         for (int i = 0; i < length; i++) {
                 root[i] = Math.sin(2 * Math.PI * i * hz / StdAudio.SAMPLE_RATE);
             }
-
-            /*
-            StdAudio.play(root);
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException ie) {
-                ie.printStackTrace();
-            }
-
-              */
-
         for (int i = 0; i < length; i++) {
                 majorThird[i] = Math.sin(2 * Math.PI * i * majorThirdHz / StdAudio.SAMPLE_RATE);
             }
-           
-            /*
-            StdAudio.play(majorThird);
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException ie) {
-                ie.printStackTrace();
-            }
-
-              */
-            
         for (int i = 0; i < length; i++) {
                 perfectFifth[i] = Math.sin(2 * Math.PI * i * perfectFifthHz / StdAudio.SAMPLE_RATE);
             }
-            
-            /*
-            StdAudio.play(perfectFifth);
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException ie) {
-                ie.printStackTrace();
-            }
-
-              */
-            
         return add3Arrays(root, majorThird, perfectFifth);
     }
+    /*
+
+    Function: Minor Chord
     
-    public static double[] majorChord(double hz, double duration){
-        double majorThirdHz = hz * Math.pow(2,4.0/12);
+    */
+    
+    public static double[] minorChord(double hz, double duration){
+        double minorThirdHz = hz * Math.pow(2,3.0/12);
         double perfectFifthHz =hz * Math.pow(2,7.0/12);
         double n = (double)StdAudio.SAMPLE_RATE;
         int length =(int)Math.ceil(n*duration);
         double[] root= new double[length];
-        double[] majorThird = new double[length];
+        double[] minorThird = new double[length];
         double[] perfectFifth = new double[length];
         for (int i = 0; i < length; i++) {
                 root[i] = Math.sin(2 * Math.PI * i * hz / StdAudio.SAMPLE_RATE);
             }
-
-            /*
-            StdAudio.play(root);
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException ie) {
-                ie.printStackTrace();
-            }
-
-              */
-
         for (int i = 0; i < length; i++) {
-                majorThird[i] = Math.sin(2 * Math.PI * i * majorThirdHz / StdAudio.SAMPLE_RATE);
+                minorThird[i] = Math.sin(2 * Math.PI * i * minorThirdHz / StdAudio.SAMPLE_RATE);
             }
-           
-            /*
-            StdAudio.play(majorThird);
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException ie) {
-                ie.printStackTrace();
-            }
-
-              */
-            
         for (int i = 0; i < length; i++) {
                 perfectFifth[i] = Math.sin(2 * Math.PI * i * perfectFifthHz / StdAudio.SAMPLE_RATE);
             }
-            
-            /*
-            StdAudio.play(perfectFifth);
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException ie) {
-                ie.printStackTrace();
-            }
-
-              */
-            
-        return add3Arrays(root, majorThird, perfectFifth);
+        return add3Arrays(root, minorThird, perfectFifth);
     }
     
     
