71ec80a5a8122ccd128a21b865ae7220044f97cd
Ethan polley <ethanpolley@Ethans-MacBook-Pro.local>
2017-12-06 11:38:52 -0500

    NICE

--- MusicLibrary.java
+++ MusicLibrary.java
@@ -1,10 +1,10 @@
 public class MusicLibrary {
     public static void main(String args[]) {
-        ArrayTools.printArray(majorChord(440,2));
+        StdAudio.play(majorChord(440,2));
     } 
 
     //Ethan's methods vv
-
+/*
     public static double[] harmonic(int pitch, double duration) {
         double hz = 440.0 * Math.pow(2, pitch / 12.0);
         double[] a  = tone(hz, duration);
@@ -13,9 +13,10 @@ public class MusicLibrary {
         double[] h  = sum(hi, lo, 0.5, 0.5);
         return ArrayTools.addArrays(a, h, 0.5, 0.5);
     }
+    */
     public static double[] majorChord(double hz, double duration){
-        double majorThirdHz = hz * Math.pow(2,4/12);
-        double perfectFifthHz =hz * Math.pow(2,7/12);
+        double majorThirdHz = hz * Math.pow(2,4.0/12);
+        double perfectFifthHz =hz * Math.pow(2,7.0/12);
         double n = (double)StdAudio.SAMPLE_RATE;
         int length =(int)Math.ceil(n*duration);
         double[] root= new double[length];
@@ -24,16 +25,100 @@ public class MusicLibrary {
         for (int i = 0; i < length; i++) {
                 root[i] = Math.sin(2 * Math.PI * i * hz / StdAudio.SAMPLE_RATE);
             }
+
+            /*
+            StdAudio.play(root);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ie) {
+                ie.printStackTrace();
+            }
+
+              */
+
         for (int i = 0; i < length; i++) {
                 majorThird[i] = Math.sin(2 * Math.PI * i * majorThirdHz / StdAudio.SAMPLE_RATE);
             }
+           
+            /*
+            StdAudio.play(majorThird);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ie) {
+                ie.printStackTrace();
+            }
 
+              */
+            
         for (int i = 0; i < length; i++) {
                 perfectFifth[i] = Math.sin(2 * Math.PI * i * perfectFifthHz / StdAudio.SAMPLE_RATE);
             }
-        return ArrayTools.addArrays(ArrayTools.addArrays(root, majorThird,.5,.5),perfectFifth,2/3,1/3);
+            
+            /*
+            StdAudio.play(perfectFifth);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ie) {
+                ie.printStackTrace();
+            }
+
+              */
+            
+        return add3Arrays(root, majorThird, perfectFifth);
     }
     
+    public static double[] majorChord(double hz, double duration){
+        double majorThirdHz = hz * Math.pow(2,4.0/12);
+        double perfectFifthHz =hz * Math.pow(2,7.0/12);
+        double n = (double)StdAudio.SAMPLE_RATE;
+        int length =(int)Math.ceil(n*duration);
+        double[] root= new double[length];
+        double[] majorThird = new double[length];
+        double[] perfectFifth = new double[length];
+        for (int i = 0; i < length; i++) {
+                root[i] = Math.sin(2 * Math.PI * i * hz / StdAudio.SAMPLE_RATE);
+            }
+
+            /*
+            StdAudio.play(root);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ie) {
+                ie.printStackTrace();
+            }
+
+              */
+
+        for (int i = 0; i < length; i++) {
+                majorThird[i] = Math.sin(2 * Math.PI * i * majorThirdHz / StdAudio.SAMPLE_RATE);
+            }
+           
+            /*
+            StdAudio.play(majorThird);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ie) {
+                ie.printStackTrace();
+            }
+
+              */
+            
+        for (int i = 0; i < length; i++) {
+                perfectFifth[i] = Math.sin(2 * Math.PI * i * perfectFifthHz / StdAudio.SAMPLE_RATE);
+            }
+            
+            /*
+            StdAudio.play(perfectFifth);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ie) {
+                ie.printStackTrace();
+            }
+
+              */
+            
+        return add3Arrays(root, majorThird, perfectFifth);
+    }
     
     
     //Charley's methods vv
@@ -43,8 +128,8 @@ public class MusicLibrary {
     }**/
     
     public static double[] add3Arrays(double[] a,double[] b,double[] c){
-        double[] h  = sum(a, b, 0.5, 0.5);
-        return ArrayTools.addArrays(c, h, 0.5, 0.5);
+        double[] h  = ArrayTools.addArrays(a, b, 0.5, 0.5);
+        return ArrayTools.addArrays(c, h, .334, .666);
     }
     
 }
