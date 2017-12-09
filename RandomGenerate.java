bda86a5d471b8b75d07d53262ddf540275f1f9d8
Ethan Polley <Ethanpolley@ethans-imac.local>
2017-12-09 18:24:25 -0500

    Random Generate

--- RandomGenerate.java
+++ RandomGenerate.java
@@ -2,18 +2,20 @@ import java.util.Random;
 public class RandomGenerate {
     public static void main(String args[]) {
         GenerateImage.generate(majorMinor(440));
+        StdAudio.play(majorMinor(320));
 
     }
 
    public static double[] majorMinor(double hz){
-    double[] root1 = MusicLibrary.harmonic(300.25,3);
-    double[] root2 = MusicLibrary.harmonic(500.99,3);
-    double[] root3 = MusicLibrary.minorChord(200,3);
-    double[] final1 = MusicLibrary.concatArray(root1,root2);
-    double[] final2 = MusicLibrary.concatArray(final1,root3);
-    double[] final3 = MusicLibrary.repeatNote(final2,5,0);
+    double[] cMaj = MusicLibrary.majorChord(hz*Math.pow(2,(12*Math.random())/12),Math.random());
+    double[] dMin = MusicLibrary.majorChord(hz*Math.pow(2,(12*Math.random())/12),Math.random());
+    double[] eMin = MusicLibrary.majorChord(hz*Math.pow(2,(12*Math.random())/12),Math.random());
+    double[] fMaj = MusicLibrary.majorChord(hz*Math.pow(2,(12*Math.random())/12),Math.random());
+    double[] gMaj = MusicLibrary.majorChord(hz*Math.pow(2,(12*Math.random())/12),Math.random());
+    double[] aMin = MusicLibrary.majorChord(hz*Math.pow(2,(12*Math.random())/12),Math.random());
+    double[] final1 = MusicLibrary.repeatNote(MusicLibrary.concatArray(cMaj,MusicLibrary.concatArray(fMaj,MusicLibrary.concatArray(dMin,MusicLibrary.concatArray(aMin,MusicLibrary.concatArray(gMaj,cMaj))))),4,0);
 
-return final3;
+return final1;
 }
 
 
