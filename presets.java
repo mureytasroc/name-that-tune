b307f2a7710c31006a3aeff3e90cf4464f2607c6
Ethan polley <ethanpolley@Ethans-MacBook-Pro.local>
2018-01-12 08:28:03 -0500

    k

--- presets.java
+++ presets.java
@@ -27,4 +27,26 @@ public static double[] alien(){
         finalie=MusicLibrary.concatArray(finalie,a);
         return (finalie);
     }
+
+public static double[] randomBeatSadInput(){
+        double[] notes={440,493.883,277.183,329.628,369.994};
+        double[] notes2=MusicLibrary.shuffleNotes(notes,8);
+        double[] startNotes=notes2;
+        double[] a =MusicLibrary.noteBeatFollow(notes2, 120, 0.5,0.9);
+        double[] finalie=a;
+        //start
+        notes2[0]=notes[(int) Math.floor(Math.random() * notes.length)];
+        notes2[1]=notes[(int) Math.floor(Math.random() * notes.length)];
+        a =MusicLibrary.noteBeatFollow(notes2, 120, 0.5,0.9);
+        finalie=MusicLibrary.concatArray(finalie,a);
+        //end
+        notes2[0]=notes[(int) Math.floor(Math.random() * notes.length)];
+        notes2[1]=notes[(int) Math.floor(Math.random() * notes.length)];
+        a =MusicLibrary.noteBeatFollow(notes2, 120, 0.5,0.9);
+        finalie=MusicLibrary.concatArray(finalie,a);
+        //--
+        a =MusicLibrary.noteBeatFollow(startNotes, 120, 0.5,0.9);
+        finalie=MusicLibrary.concatArray(finalie,a);
+        return (finalie);
+    }
 }
